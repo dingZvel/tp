9de76eaf4c9d43cf249eb8be30a5e33afa2752d2
dingZvel <zhezhe0214@gmail.com>
2025-04-08 05:57:43 +0800

    Fixed some tests which were disabled
    Remaining disabled tests are in EditcommandTest.java,
    AddCommandParserTest.java, AddressBookParserTest.java,
    LogicManagerTest.java, and UniqueClientListTest.java.

--- src/test/java/seedu/address/logic/commands/EditCommandTest.java
+++ src/test/java/seedu/address/logic/commands/EditCommandTest.java
@@ -52,7 +52,6 @@ public class EditCommandTest {
     }
 
     @Test
-    @Disabled
     //TODO: Fix this test
     public void execute_someFieldsSpecifiedUnfilteredList_success() {
         Index indexLastClient = Index.fromOneBased(model.getSortedFilteredClientList().size());
@@ -75,7 +74,6 @@ public class EditCommandTest {
     }
 
     @Test
-    @Disabled
     public void execute_noFieldSpecifiedUnfilteredList_success() {
         EditCommand editCommand = new EditCommand(INDEX_FIRST_PERSON, new EditClientDescriptor());
         Client editedClient = model.getSortedFilteredClientList().get(INDEX_FIRST_PERSON.getZeroBased());
@@ -88,7 +86,6 @@ public class EditCommandTest {
     }
 
     @Test
-    @Disabled
     //TODO: Fix this test
     public void execute_filteredList_success() {
         showClientAtIndex(model, INDEX_FIRST_PERSON);
--- src/test/java/seedu/address/testutil/ClientUtil.java
+++ src/test/java/seedu/address/testutil/ClientUtil.java
@@ -4,6 +4,8 @@ import static seedu.address.logic.parser.CliSyntax.PREFIX_ADDRESS;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_EMAIL;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_NAME;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_PHONE;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_PREFERENCE;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_PRIORITY;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_TAG;
 
 import java.util.Set;
@@ -11,6 +13,8 @@ import java.util.Set;
 import seedu.address.logic.commands.AddCommand;
 import seedu.address.logic.commands.EditCommand.EditClientDescriptor;
 import seedu.address.model.client.Client;
+import seedu.address.model.client.Priority;
+import seedu.address.model.client.ProductPreference;
 import seedu.address.model.tag.Tag;
 
 /**
@@ -37,6 +41,10 @@ public class ClientUtil {
         client.getTags().stream().forEach(
             s -> sb.append(PREFIX_TAG + s.tagName + " ")
         );
+        sb.append(PREFIX_PREFERENCE + client.getProductPreference().map(ProductPreference::toString).orElse("")
+                + " ");
+        //TODO: modify this line below
+        sb.append(PREFIX_PRIORITY + client.getPriority().map(Priority::toString).orElse("") + " ");
         return sb.toString();
     }
 
