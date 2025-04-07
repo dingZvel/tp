b60ce3abf6a8f05b1cfb9aacff3357d482c22471
SherrinfordHR <sherryhrobin@gmail.com>
2025-04-07 23:56:58 +0800

    Enabling all tests

--- src/test/java/seedu/address/model/AddressBookTest.java
+++ src/test/java/seedu/address/model/AddressBookTest.java
@@ -13,7 +13,6 @@ import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import javafx.collections.FXCollections;
