2f97451e8a1750869e8f658abfc67b51cb2be669
SherrinfordHR <sherryhrobin@gmail.com>
2025-04-08 00:12:50 +0800

    Update EmailTest.java

--- src/test/java/seedu/address/model/client/EmailTest.java
+++ src/test/java/seedu/address/model/client/EmailTest.java
@@ -50,14 +50,12 @@ public class EmailTest {
         assertFalse(Email.isValidEmail("peterjack@example.com.")); // domain name ends with a period
         assertFalse(Email.isValidEmail("peterjack@-example.com")); // domain name starts with a hyphen
         assertFalse(Email.isValidEmail("peterjack@example.com-")); // domain name ends with a hyphen
-        assertFalse(Email.isValidEmail("peterjack@example.c")); // top level domain has less than two chars
-
         // valid email
         assertTrue(Email.isValidEmail("PeterJack_1190@example.com")); // underscore in local part
         assertTrue(Email.isValidEmail("PeterJack.1190@example.com")); // period in local part
         assertTrue(Email.isValidEmail("PeterJack+1190@example.com")); // '+' symbol in local part
         assertTrue(Email.isValidEmail("PeterJack-1190@example.com")); // hyphen in local part
-        assertTrue(Email.isValidEmail("a@bc.de")); // minimal
+        assertTrue(Email.isValidEmail("a@b.c")); // minimal
         assertTrue(Email.isValidEmail("test@localhost.com")); // alphabets only
         assertTrue(Email.isValidEmail("123@145.com")); // numeric local part and domain name
         assertTrue(Email.isValidEmail("a1+be.d@example1.com")); // mixture of alphanumeric and special characters
